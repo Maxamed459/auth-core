9b3330248cbde8c321516a8cf45aa3df7ff47c51
Maxamed459 <maxamedmahdi459@gmail.com>
2026-08-04 23:02:26 +0300

    remove MongoDB_URI reference and update dotenv configuration for database connection

--- backend/src/config/dotenv.ts
+++ backend/src/config/dotenv.ts
@@ -3,16 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const DATABASE_URL = process.env.DATABASE_URL;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
 export const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN;
 export const JWT_ACCESS_EXPIRES_IN = process.env.JWT_ACCESS_EXPIRES_IN;
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
-
-if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI is not set");
-}
+export const RESEND_API_KEY = process.env.RESEND_API_KEY;
+export const EMAIL_FROM = process.env.EMAIL_FROM;
 export const LOG_LEVEL = process.env.LOG_LEVEL;
 export const NODE_ENV = process.env.NODE_ENV;
 export const MAX_LIMIT = Number(process.env.MAX_LIMIT || "100");
